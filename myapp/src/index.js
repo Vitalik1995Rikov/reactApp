558955a213c27a17bcc8478eb760ed09026890b8
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-06 22:57:11 +0300

    gurufy3

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,43 +1,20 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css"
 
-const Title = props => {
-  const [isReversed, reverse] = useState(null)
-  const [color, setColor] = useState(0)
+const Collapse = () => {
+  const [isOpen, setOpen] = React.useState(true);
 
-  const [renderState, setRenderState] = useState("")
-  const [renderCount, setRenderCount] = useState(1)
-
-  const colors = ["#001f3f", "#39cccc"]
-  const styles = { color: colors[color] }
-
-  const onClickHandle = event => {
-    reverse(isReversed => !isReversed)
-    setColor(1)
+  const onClickHandle = function () {
+    setOpen(isOpen => !isOpen)
   }
 
-  useEffect(() => {
-    setRenderState("Произошел первый render в DOM")
-  }, [])
-
-  useEffect(() => {
-    if (isReversed !== null) {
-      setRenderState(`Произошел rerender в DOM ${renderCount} раз`)
-      setRenderCount(count => count + 1)
-    }
-  }, [isReversed])
-
   return (
-    <p onClick={onClickHandle} style={styles}>
-      {
-        isReversed 
-          ? props.text.split("").reverse().join("") 
-          : props.text
-      } ({renderState})
-    </p>
+    <div>
+      <button>
+      </button>
+    </div>
   )
-}
 
 ReactDOM.render(
     <div>
