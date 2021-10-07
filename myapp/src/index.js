fac5bb10867f28fee53b557eadea62460a07968c
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-07 21:47:59 +0300

    gurufy5

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,30 +1,45 @@
-import React from 'react';
+import React, {createContext, useContext} from 'react';
 import ReactDOM from 'react-dom';
-import "./index.css"
 
-const Collapse = () => {
-  const [isOpen, setOpen] = React.useState(true);
+const testContext = createContext()
 
-  const onClickHandle = function () {
-    setOpen(isOpen => !isOpen)
-  }
+const ComponentC = (props) => {
+  const context = useContext(testContext)
+  const styles = {color: context.color}
 
   return (
-    <div className="collapse">
-      <button onClick={onClickHandle}>
-        {isOpen ? "Скрыть" : "Показать"}
-      </button>
-      <div className={"content " + (isOpen ? "open" : "close")}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat.
-      </div>
+    <p style={styles}>ComponentC</p>
+  )
+}
+
+const ComponentB = () => {
+  return (
+    <div>
+      <p>ComponentB</p>
+      <ComponentC/>
     </div>
   )
 }
 
+const {Provider} = testContext
+
+const ComponentA = () => {
+  const color = "#31c5b2"
+  const contextValue = {color}
+
+  return (
+    <Provider value={contextValue}>
+    <div>
+      <p>ComponentA</p>
+      <ComponentB/>
+    </div>
+    </Provider>
+  )
+}
+
+
+
 ReactDOM.render(
-    <Collapse/>,
+    <ComponentA/>,
   document.getElementById('root')
 );
