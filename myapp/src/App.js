7fef762d5e4a59d50a74f4b2e2687597ccc497da
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-12 21:33:59 +0300

    gurufy15

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import Basket from "./components/Basket";
+import Products from "./components/Products";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="app">
+            <Products/>
+            <Basket/>
+        </div>
+    )
+}
+
+export default App;
