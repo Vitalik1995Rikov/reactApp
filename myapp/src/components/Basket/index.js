a92ed39553bcd708c73830969dced6b199ebca90
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-12 21:25:27 +0300

    gurufy14

--- myapp/src/components/Basket/index.js
+++ myapp/src/components/Basket/index.js
@@ -5,4 +5,8 @@ export default function Basket() {
     const products = useSelector((store) => store.items);
     const price = products
         .filter((product) => !!product.count)
+        .map((product) => product.price * product.count)
+        .reduce((a, b) => a + b, 0);
+
+        return price ? (<button className="basket">Перейти к оплате(${price})</button>) : null;
 }
