80ca60dd28c7e290839fa942f90c33ad51d22e62
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-20 14:42:01 +0300

    Petrichenko4

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const text = 'Clean project'
-
-const elem = (
-  <div>
-    <h2>{text}</h2>
-    <input type="text"/>
-    <button>Click</button>
-  </div>
-);
+import App from './App'
 
 ReactDOM.render(
-    elem,
+    <App/>,
   document.getElementById('root')
 );
