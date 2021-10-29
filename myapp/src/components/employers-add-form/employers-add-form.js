4ed97db6bf4c5151308f48f1acf11154bbe95ba9
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-29 12:47:14 +0300

    Petrichenko41

--- myapp/src/components/employers-add-form/employers-add-form.js
+++ myapp/src/components/employers-add-form/employers-add-form.js
@@ -18,12 +18,23 @@ class EmployersAddForm extends Component {
     }
 
     render() {
+        const {name, salary} = this.state;
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form className="add-form d-flex">
-                    <input type="text" className="form-control new-post-label" placeholder="Как его зовут?" name="name" onChange={this.onValueChange}/>
-                    <input type="number" className="form-control new-post-label" placeholder="З/П в $?" name="salary" onChange={this.onValueChange}/>
+                    <input type="text" 
+                        className="form-control new-post-label" 
+                        placeholder="Как его зовут?" 
+                        name="name" 
+                        value={name} 
+                        onChange={this.onValueChange}/>
+                    <input type="number" 
+                        className="form-control new-post-label" 
+                        placeholder="З/П в $?" 
+                        name="salary"
+                        value={salary} 
+                        onChange={this.onValueChange}/>
                     <button type="submit" className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
