2dc2922c8cb2e8d5ceb840afdaa30c3d38df4072
Vitalik1995Rikov <vitalic_rikov@mail.ru>
2021-10-29 12:36:53 +0300

    Petrichenko39

--- myapp/src/components/employers-add-form/employers-add-form.js
+++ myapp/src/components/employers-add-form/employers-add-form.js
@@ -1,16 +1,20 @@
+import { Component } from 'react';
+
 import './employers-add-form.css';
 
-function EmployersAddForm() {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form className="add-form d-flex">
-                <input type="text" className="form-control new-post-label" placeholder="Как его зовут?"/>
-                <input type="number" className="form-control new-post-label" placeholder="З/П в $?"/>
-                <button type="submit" className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+class EmployersAddForm extends Component {
+    render() {
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form className="add-form d-flex">
+                    <input type="text" className="form-control new-post-label" placeholder="Как его зовут?"/>
+                    <input type="number" className="form-control new-post-label" placeholder="З/П в $?"/>
+                    <button type="submit" className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployersAddForm;
